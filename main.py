a34527a59d8b21eae3c8f46a8359cb4935f90c23
EdgarPinto01 <108126741+EdgarPinto01@users.noreply.github.com>
2024-04-11 01:59:30 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-print("Welcomw to the tip calculator")
-bill = float(input("what is the total bill?"))
-people = int(input("how many to split the bill with?"))
-tip = int(input("what percentage tip would you like to give?"))
-billwtip = tip / 100 * bill + bill
-eachpp = billwtip / people
-print(billwtip)
-print(eachpp)
